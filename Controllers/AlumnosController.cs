99700741ac62a7acb2aa218cab8b9dbf4bcb2378
Hector Sanchez <20186183@itla.edu.do>
2020-03-01 18:01:24 -0400

    Initial commit (View & Controllers)

---
+++ Controllers/AlumnosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Universidad_ASP.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        public IActionResult IndexAlumnos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MaestriasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Universidad_ASP.Controllers
+{
+    public class MaestriasController : Controller
+    {
+        public IActionResult IndexMaestrias()
+        {
+            return View();
+        }
+    }
+}
